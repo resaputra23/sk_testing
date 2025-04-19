22080c011c34964eb371d3335cc3af8d5c809e94
resaputra23 <resaputra23@gmail.com>
2025-04-19 15:56:33 +0000

    setting for vm10.14.4.81

---
+++ counter.c
@@ -0,0 +1,33 @@
+//go:build ignore
+
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+#include <bpf/bpf_helpers.h>
+
+struct {
+    __uint(type, BPF_MAP_TYPE_ARRAY); 
+    __type(key, __u32);
+    __type(value, __u64);
+    __uint(max_entries, 1);
+} packet_count_map SEC(".maps"); 
+
+SEC("filter")
+int packet_count(struct __sk_buff *skb) {
+    //initial key
+    __u32 key = 0;
+    __u64 *count;
+
+    //retireve current packet
+    count = bpf_map_lookup_elem(&packet_count_map, &key);
+    if (count) {
+       __sync_fetch_and_add(count, 1); 
+    } else {
+        long initial_count = 1;
+        bpf_map_update_elem(&packet_count_map, &key, &initial_count, BPF_ANY);    
+    }
+    
+    return BPF_OK;
+}
+char __license[] SEC("license") = "Dual MIT/GPL";
---
+++ go.mod
@@ -0,0 +1,8 @@
+module skb_count
+
+go 1.24.2
+
+require (
+	github.com/cilium/ebpf v0.18.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+)
